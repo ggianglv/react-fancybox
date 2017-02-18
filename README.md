591692b856ba0a49b8733c9ed87561f44ba26563
vagrant <gianglevan94@gmail.com>
2017-02-18 02:29:20 +0000

    0.0.12

--- README.md
+++ README.md
@@ -4,10 +4,6 @@ __COMPONENT DESCRIPTION GOES HERE__
 
 ## Installation
 
-The easiest way to use react-fancybox is to install it from NPM and include it in your own React build process (using [Browserify](http://browserify.org), [Webpack](http://webpack.github.io/), etc).
-
-You can also use the standalone build by including `dist/react-fancybox.js` in your page. If you use this, make sure you have already included React, and it is available as a global variable.
-
 ```
 npm install react-fancybox --save
 ```
@@ -15,26 +11,12 @@ npm install react-fancybox --save
 
 ## Usage
 
-__EXPLAIN USAGE HERE__
+__BASIC USAGE__
 
 ```
 import Fancybox from 'react-fancybox';
 
-<ReactFancybox imageUrl={message.body}/>
+<ReactFancybox imageUrl={url}/>
 ```
 
-### Properties
-
-* __DOCUMENT PROPERTIES HERE__
-
-### Notes
-
-__ADDITIONAL USAGE NOTES__
-
-
-## License
-
-__PUT LICENSE HERE__
-
-Copyright (c) 2017 Giang Le.
 
--- src/Fancybox.js
+++ src/Fancybox.js
@@ -1,57 +1,59 @@
 import React from 'react';
 class Fancybox extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      showBox: false,
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			showBox: false,
+		};
+	}
 
-  showBox(event) {
-    event.preventDefault();
-    this.setState({
-      showBox: true,
-    });
-  }
+	showBox(event) {
+		event.preventDefault();
+		this.setState({
+			showBox: true,
+		});
+	}
 
-  onBoxClick(event) {
-    event.preventDefault();
-    if (event.target != this.refs.imageInBox) {
-      this.setState({
-        showBox: false,
-      });
-    }
-  }
+	onBoxClick(event) {
+		event.preventDefault();
+		if (event.target != this.refs.imageInBox) {
+			this.setState({
+				showBox: false,
+			});
+		}
+	}
 
-  render() {
-    let box = '';
-    if (this.state.showBox) {
-      box = (
-        <div onClick={this.onBoxClick.bind(this)} ref='box' className='fancy-box'>
-          <img ref='imageInBox' src={this.props.imageUrl}/>
-          <div>{this.props.caption}</div>
-        </div>
-      );
-    }
-    return (
-      <div>
-        <img onClick={this.showBox.bind(this)} src={this.props.imageUrl} className={this.props.imageClassName}/>
-        {box}
-      </div>
-    );
-  }
+	render() {
+		let box = '';
+		if (this.state.showBox) {
+			box = (
+				<div onClick={this.onBoxClick.bind(this)} ref='box' className={this.props.boxClassName}>
+					<img ref='imageInBox' src={this.props.imageUrl}/>
+					<div>{this.props.caption}</div>
+				</div>
+			);
+		}
+		return (
+			<div>
+				<img onClick={this.showBox.bind(this)} src={this.props.imageUrl} className={this.props.imageClassName}/>
+				{box}
+			</div>
+		);
+	}
 }
 
 Fancybox.propTypes = {
+	boxClassName: React.PropTypes.string,
 	caption: React.PropTypes.string,
-  imageClassName: React.PropTypes.string,
-  imageUrl: React.PropTypes.string,
+	imageClassName: React.PropTypes.string,
+	imageUrl: React.PropTypes.string,
 };
 
 Fancybox.defaultProps = {
-  caption: '',
-  imageClassName: 'fancybox-img',
-  imageUrl: '',
+	caption: '',
+	imageClassName: 'fancybox-img',
+	imageUrl: '',
+	boxClassName: 'fancybox',
 };
 
 export default Fancybox;
